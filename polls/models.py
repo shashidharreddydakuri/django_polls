69f5c020599858539a162aa892a6cfea4b0d048f
shashidharreddydakuri <d.shashidhar1996@gmail.com>
2021-09-22 23:33:57 +0530

    MODELS trying

--- polls/models.py
+++ polls/models.py
@@ -33,3 +33,23 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title        
+
+
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    publications = models.ManyToManyField(Publication)
+
+    class Meta:
+        ordering = ['headline']
+
+    def __str__(self):
+        return self.headline
